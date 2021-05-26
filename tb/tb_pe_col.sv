85802cc2568f150e923141ed39d60f19b63ca9ac
yong-hardware-server <dracol@dgist.ac.kr>
2021-05-26 11:32:31 +0900

    bf16 simple matrix-vector mult

--- tb/tb_pe_col.sv
+++ tb/tb_pe_col.sv
@@ -5,7 +5,7 @@
 // Description: Testbench for bfp16 PE columns, DEPTH = 2
 
 // TestCase: Matrix X vector
-// input	 : 4x2 ifmap 2x1 weight
+// input	 : 4x8 ifmap 8x1 weight
 
 
 // Note: Rough test 
@@ -14,70 +14,87 @@
 module tb_pe_col ();
 
 	parameter DATA_TYPE = 16; // data type width
-	parameter NUM_TEST = 8;
+	parameter NUM_TEST = 24;
 
 	reg clk = 0;
 	reg rst = 1;
 
-	reg [NUM_TEST*DATA_TYPE-1:0] input_ifmap [0:1] = 
-		// 4X2 ifmap matrix
-		////////////////////////////
-    // 3(0x4040)		8(0x4100)	//
-		// 3						8					//
-		// 3						8					//
-		// 3						8					//
-		////////////////////////////
-    {128'h0000_0000_4040_4040_4040_4040_0000_0000, 128'h0000_0000_0000_4100_4100_4100_4100_0000};
+	reg [NUM_TEST*DATA_TYPE-1:0] input_ifmap [0:7] = 
+		// 4X8 ifmap matrix
+		//////////////////////////////
+    // 1(0x3f80)	1	1	1	1	1	1	1 // 	
+    // 1(0x3f80)	1	1	1	1	1	1	1 // 	
+    // 1(0x3f80)	1	1	1	1	1	1	1 // 	
+    // 1(0x3f80)	1	1	1	1	1	1	1 // 	
+		//////////////////////////////
+    {384'h0000_0000_0000_0000_0000_0000_0000_0000_3f80_3f80_3f80_3f80_3f80_3f80_3f80_3f80_0000_0000_0000_0000_0000_0000_0000_0000,
+		 384'h0000_0000_0000_0000_0000_0000_0000_3f80_3f80_3f80_3f80_3f80_3f80_3f80_3f80_0000_0000_0000_0000_0000_0000_0000_0000_0000,
+		 384'h0000_0000_0000_0000_0000_0000_3f80_3f80_3f80_3f80_3f80_3f80_3f80_3f80_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000,
+		 384'h0000_0000_0000_0000_0000_3f80_3f80_3f80_3f80_3f80_3f80_3f80_3f80_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000,
+		 384'h0000_0000_0000_0000_3f80_3f80_3f80_3f80_3f80_3f80_3f80_3f80_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000,
+		 384'h0000_0000_0000_3f80_3f80_3f80_3f80_3f80_3f80_3f80_3f80_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000,
+		 384'h0000_0000_3f80_3f80_3f80_3f80_3f80_3f80_3f80_3f80_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000,
+		 384'h0000_3f80_3f80_3f80_3f80_3f80_3f80_3f80_3f80_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000};
     
 	reg [NUM_TEST*DATA_TYPE-1:0] input_weight = 
-		// 2X1 weight vector
+		// 8X1 weight vector
 		////////////////////
     // 1 		(0x3F80)	//
-		// 1240 (0x449B)	//
+    // 1 		(0x3F80)	//
+    // 1 		(0x3F80)	//
+    // 1 		(0x3F80)	//
+    // 1 		(0x3F80)	//
+    // 1 		(0x3F80)	//
+    // 1 		(0x3F80)	//
+    // 1 		(0x3F80)	//
 		////////////////////
-		128'h449B_3F80_0000_0000_0000_0000_0000_0000;	
+		384'h0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_3f80_3f80_3f80_3f80_3f80_3f80_3f80_3f80;
 
-	reg [NUM_TEST-1:0] input_ctrl = 8'b0011_1110;
 
-	reg [2*DATA_TYPE-1:0] ifmap;
+	reg [NUM_TEST-1:0] input_ctrl = 24'b1111_1111_1111_1111_0000_0000;
+
+	reg [8*DATA_TYPE-1:0] ifmap;
 	reg [DATA_TYPE-1:0] 	weight;
 	reg	ctrl;
 	// reg counter;
 
-	reg [2*DATA_TYPE-1:0] out_ifmap;
+	reg [8*DATA_TYPE-1:0] out_ifmap;
 	reg [DATA_TYPE-1:0] out;  
 	// 4X1 output
 	////////////////////
-	//	9935 (0x461B)	//
-	//	9935 (0x461B)	//	
-	//  9935 (0x461B)	//
-	//  9935 (0x461B)	//
+	//	8 (0x4100)		//
+	//	8 (0x4100)		//
+	//	8 (0x4100)		//
+	//	8 (0x4100)		//
 	////////////////////	
 
 	// Generate simulation clock
 	always #5 clk = !clk;
 
-	reg [2:0] counter = 3'b111;	
+	reg [5:0] counter;
+
 
 	always @ ( posedge clk ) begin
-		counter <= counter - 1'b1;
 		ctrl 	  <= input_ctrl[counter];
-		ifmap   <= {input_ifmap[0][16*(counter+1)-1 -: 16], input_ifmap[1][16*(counter+1)-1 -: 16]};
-		weight  <= input_weight[16*(counter+1)-1 -: 16];		
-	end
+		ifmap   <= {input_ifmap[0][16*counter +: 16], input_ifmap[1][16*counter +: 16],
+								input_ifmap[2][16*counter +: 16], input_ifmap[3][16*counter +: 16],
+								input_ifmap[4][16*counter +: 16], input_ifmap[5][16*counter +: 16],
+								input_ifmap[6][16*counter +: 16], input_ifmap[7][16*counter +: 16]};
+		weight  <= input_weight[16*counter +: 16];		
+	
 	// set the input values per clock cycle 
 	/*
 	always @ (posedge clk) begin
 		ctrl 	 = input_ctrl[counter];
 		ifmap  = input_ifmap[0:1][16*(counter+1)-1:16*counter];
 		weight = input_weight[16*(counter+1)-1:16*counter];
-
+	*/
 		if (counter < NUM_TEST-1) begin
 			counter = counter + 1'b1;
 		end else begin
 			counter = 'd0;
 		end
-	end */
+	end
 
 	// instantiate system
 	bfp16_pe_col my_col(
--- vmod/bfp16_pe_col.v
+++ vmod/bfp16_pe_col.v
@@ -27,7 +27,7 @@
 
 
 module bfp16_pe_col # (
-												parameter DEPTH = 2 ) (
+												parameter DEPTH = 8 ) (
 												clk,
 												rst,
 												ctrl,
@@ -41,30 +41,98 @@ module bfp16_pe_col # (
 	input  rst;
 	input  ctrl;
 	input  [15:0]	weight;
-	input  [31:0] ifmap;
-	output [31:0] out_ifmap;
+	input  [16*DEPTH-1:0] ifmap;
+	output [16*DEPTH-1:0] out_ifmap;
 	output [15:0] out;
 
-	wire	 [15:0] bridge;
+	wire	 [15:0] bridge7;
+	wire	 [15:0] bridge6;
+	wire	 [15:0] bridge5;
+	wire	 [15:0] bridge4;
+	wire	 [15:0] bridge3;
+	wire	 [15:0] bridge2;
+	wire	 [15:0] bridge1;
 
-	bfp16_pe_wsNonOpt pe0(
+
+
+	bfp16_pe_wsNonOpt pe7(
 		.clk(clk),
 		.rst(rst),
 		.ctrl(ctrl),
 		.in(weight),
-		.in_ifmap(ifmap[16*DEPTH-1:16]),
-		.out(bridge),
-		.out_ifmap(out_ifmap[16*DEPTH-1:16])
+		.in_ifmap(ifmap[16*(DEPTH-1)+:16]),
+		.out(bridge7),
+		.out_ifmap(out_ifmap[16*(DEPTH-1)+:16])
+	);
+
+	bfp16_pe_wsNonOpt pe6(
+		.clk(clk),
+		.rst(rst),
+		.ctrl(ctrl),
+		.in(bridge7),
+		.in_ifmap(ifmap[16*(DEPTH-2)+:16]),
+		.out(bridge6),
+		.out_ifmap(out_ifmap[16*(DEPTH-2)+:16])
+	);
+
+	bfp16_pe_wsNonOpt pe5(
+		.clk(clk),
+		.rst(rst),
+		.ctrl(ctrl),
+		.in(bridge6),
+		.in_ifmap(ifmap[16*(DEPTH-3)+:16]),
+		.out(bridge5),
+		.out_ifmap(out_ifmap[16*(DEPTH-3)+:16])
+	);
+
+	bfp16_pe_wsNonOpt pe4(
+		.clk(clk),
+		.rst(rst),
+		.ctrl(ctrl),
+		.in(bridge5),
+		.in_ifmap(ifmap[16*(DEPTH-4)+:16]),
+		.out(bridge4),
+		.out_ifmap(out_ifmap[16*(DEPTH-4)+:16])
+	);
+
+	bfp16_pe_wsNonOpt pe3(
+		.clk(clk),
+		.rst(rst),
+		.ctrl(ctrl),
+		.in(bridge4),
+		.in_ifmap(ifmap[16*(DEPTH-5)+:16]),
+		.out(bridge3),
+		.out_ifmap(out_ifmap[16*(DEPTH-5)+:16])
+	);
+
+	bfp16_pe_wsNonOpt pe2(
+		.clk(clk),
+		.rst(rst),
+		.ctrl(ctrl),
+		.in(bridge3),
+		.in_ifmap(ifmap[16*(DEPTH-6)+:16]),
+		.out(bridge2),
+		.out_ifmap(out_ifmap[16*(DEPTH-6)+:16])
 	);
 
 	bfp16_pe_wsNonOpt pe1(
 		.clk(clk),
 		.rst(rst),
 		.ctrl(ctrl),
-		.in(bridge),
-		.in_ifmap(ifmap[16*(DEPTH-1)-1:0]),
+		.in(bridge2),
+		.in_ifmap(ifmap[16*(DEPTH-7)+:16]),
+		.out(bridge1),
+		.out_ifmap(out_ifmap[16*(DEPTH-7)+:16])
+	);
+
+	bfp16_pe_wsNonOpt pe0(
+		.clk(clk),
+		.rst(rst),
+		.ctrl(ctrl),
+		.in(bridge1),
+		.in_ifmap(ifmap[16*(DEPTH-8)+:16]),
 		.out(out),
-		.out_ifmap(out_ifmap[16*(DEPTH-1)-1:0])
+		.out_ifmap(out_ifmap[16*(DEPTH-8)+:16])
 	);
 
 endmodule
